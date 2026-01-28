f6fd892204d356d5649127ed3e3772e69c5d93ca
Name2215 <name222office@gmail.com>
2026-01-29 03:59:46 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,17 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//〇追加
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var ynRouter = require('./routes/yn');
+var qrRouter = require('./routes/qr');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/qr', qrRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,11 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//〇追加
-app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
-app.use('/cat',catRouter);
-app.use('/yn',ynRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,5 +49,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
